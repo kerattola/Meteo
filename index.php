b0e3f982241980e74ba431de1b96478b214a70f1
ivanna2206 <75306669+ivanna2206@users.noreply.github.com>
2020-12-02 10:35:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+require_once ("./layout/right_menu.php");
+
+$file = './views/'.$_GET['action'].'.php';
+if (isset($_GET['action']) && file_exists($file)) {
+	require_once $file;
+	if (($file != './views/login.php')&&($file != './views/registration.php')&&($file != './views/newdish.php')) {
+		require_once ("./layout/footer.php");
+    }
+}
+else{
+	require_once ("./layout/header.php");
+	require_once ("./views/main.php");
+	require_once ("./layout/footer.php");
+}
+
+?>
